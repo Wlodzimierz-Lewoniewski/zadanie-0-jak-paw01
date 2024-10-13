2d35c8e06a860755b7253254cd4a20f25c05c652
jak-paw01 <jakub.pawlak79@gmail.com>
2024-10-13 19:17:09 +0200

    test programu wyszukiwawczego

--- main.py
+++ main.py
@@ -1,9 +1,59 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import string
+from collections import defaultdict
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+def wyczysc_dokument(dokument):
+    # kod analogiczny do wskazówki z prezentacji
+    znaki_interpunkcyjne = string.punctuation 
+    slowa = ''.join([znak if znak not in znaki_interpunkcyjne else ' ' for znak in dokument])
+    return slowa.lower().split() 
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+# input
+liczba_dokumentow = int(input())
+
+dokumenty = []
+for dok in range(liczba_dokumentow):
+    dokument = input().strip()
+    dokumenty.append(dokument)
+
+liczba_zapytan = int(input())
+
+zapytania = []
+for zap in range(liczba_zapytan):
+    zapytanie = input().strip()
+    zapytania.append(zapytanie)
+
+dokumenty_licz_slowa = []
+
+for dokument in dokumenty:
+    slowa = wyczysc_dokument(dokument)
+    licznik_slow = defaultdict(int) # uzycie kodu z prezentacji
+    for slowo in slowa:
+        licznik_slow[slowo] += 1 
+    dokumenty_licz_slowa.append(licznik_slow)
+
+# sprawdzenie zapytan oraz ustalenie kolejnosci
+wyniki = []
+
+for zapytanie in zapytania:
+    dokumenty_zapytania = []
+    for index, licznik_slow in enumerate(dokumenty_licz_slowa):
+        liczba_wystapien = licznik_slow.get(zapytanie.lower(), 0)
+        if liczba_wystapien > 0:
+            dokumenty_zapytania.append((index, liczba_wystapien))
+    
+    
+    dokumenty_zapytania.sort(key=lambda x: (-x[1], x[0]))
+    
+    indeksy = [] 
+    
+    for dokument in dokumenty_zapytania:
+        indeks_dokumentu = dokument[0]  
+        indeksy.append(str(indeks_dokumentu))  
+    
+    wynik = "[" + ", ".join(indeksy) + "]"
+    wyniki.append(wynik)
+
+# prezentacja wyniku 
+for wynik in wyniki:
+    print(wynik)
+
